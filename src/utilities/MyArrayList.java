5f99de24c9a78ca62d106d0a2df1b5ff46786e8c
Mikael Ly <mikael2aa@gmail.com>
2025-11-04 02:00:02 -0700

    implemented remove(int index)
    - Added remove(int index). currently untested
    
    - Added the main data structure + constructor (ArrayList<E> list and the constructor method MyArrayList())
    
    - added import ArrayList
    
    - moved @override next to method names to follow best practices

--- src/utilities/MyArrayList.java
+++ src/utilities/MyArrayList.java
@@ -1,5 +1,7 @@
 package utilities;
 
+import java.util.ArrayList;
+
 /**
  * MyArrayList.java
  * 
@@ -11,128 +13,166 @@ package utilities;
  * 
  */
 public class MyArrayList<E> implements ListADT<E> {
+	
+	/**
+	 * This is the ArrayList that will be used for this ADT.
+	 * All items/nodes will be stored here.
+	 */
+	private ArrayList<E> list;
+	
+	/**
+	 * Constructor Method
+	 * Initialize the list by creating an ArrayList<E>.
+	 */
+	public MyArrayList() {
+		this.list = new ArrayList<E>(); 
+	}
 
-	@Override
+	
 	/**
 	 * 
 	 */
+	@Override
 	public int size() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
 	/**
 	 * 
 	 */
+	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
+	
 	/**
 	 * 
 	 */
+	@Override
 	public boolean add(int index, E toAdd) throws NullPointerException, IndexOutOfBoundsException {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	/**
 	 * 
 	 */
+	@Override
 	public boolean add(E toAdd) throws NullPointerException {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	/**
 	 * 
 	 */
+	@Override
 	public boolean addAll(ListADT<? extends E> toAdd) throws NullPointerException {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	/**
 	 * 
 	 */
+	@Override
 	public E get(int index) throws IndexOutOfBoundsException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	/**
-	 * @todo To be done by: Mikael
+	 * This method removes an item from the ArrayList given an index.
+	 * Index must be greater than 0 and no greater than the ArrayList size.
+	 * <p>
+	 * Returns the data stored in the item that was removed.
+	 * @author Mikael Ly
+	 * @date 11/4/2025
+	 * 
+	 * @param index - index of the item to be removed
+	 * @return data of the item that was removed
 	 */
+	@Override
 	public E remove(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
+		// If the index is below 0 or greater than the amount of items in the list,
+		// throw an IndexOutOfBoundsException.
+		if (index < 0 || index >= this.list.size()) {
+			throw new IndexOutOfBoundsException("Index is out of bounds. Index: " + index 
+											+ " should be between 0 - " + this.list.size());
+		}
+		// If index is valid, go ahead with removing the item.
+		// Store item data to be returned
+		E temp = this.list.get(index);
+		
+		// Remove the item
+		this.list.remove(index);
+		
+		return temp; // return the data that was removed from the list
 	}
 
-	@Override
 	/**
 	 * @todo To be done by: Mikael
 	 */
+	@Override
 	public E remove(E toRemove) throws NullPointerException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	/**
 	 * 
 	 */
+	@Override
 	public E set(int index, E toChange) throws NullPointerException, IndexOutOfBoundsException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	/**
 	 * @todo To be done by: Mikael
 	 */
+	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	/**
 	 * 
 	 */
+	@Override
 	public boolean contains(E toFind) throws NullPointerException {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	/**
 	 * @todo To be done by: Mikael
 	 */
+	@Override
 	public E[] toArray(E[] toHold) throws NullPointerException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	/**
 	 * @todo To be done by: Mikael
 	 */
+	@Override
 	public Object[] toArray() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	/**
+	 * Utilize the iterator class
 	 * 
+	 * @return an Iterator<E> based off of this list
 	 */
+	@Override
 	public Iterator<E> iterator() {
 		// TODO Auto-generated method stub
 		return null;
