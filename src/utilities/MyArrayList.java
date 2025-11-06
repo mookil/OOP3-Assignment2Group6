0d045929363435e9b5bc634f219a2b2453c1716d
Mikael Ly <mikael2aa@gmail.com>
2025-11-05 17:43:57 -0700

    Restructured MyArrayList + finished multiple functions
    - MyArrayList no longer utilizes the ArrayList library to adhere to assignment requirements, it now uses an Object[]
    
    - Added an Object[] list, DEFAULT_SIZE constant, and int size.
    
    - Added constructors
    
    - Implemented the get(), remove(), and isEmpty() functions
    
    - Added author's + header description

--- src/utilities/MyArrayList.java
+++ src/utilities/MyArrayList.java
@@ -5,9 +5,15 @@ import java.util.ArrayList;
 /**
  * MyArrayList.java
  * 
- * @author 
- * @version 1.0
+ * This is an ADT imitating the ArrayList library in java.util.*. 
+ * Implements the ListADT<E> from ListADT.java, along with all given functions.
+ * Stores all items/nodes within an Object[].
+ * This ADT should be of a flexible size, capable of adding/removing with no size limit.
+ * <p>
  * 
+ * 
+ * @author Mikael Ly, Christopher Hanlon
+ * @version 1.0
  * <p>
  * 
  * 
@@ -15,17 +21,48 @@ import java.util.ArrayList;
 public class MyArrayList<E> implements ListADT<E> {
 	
 	/**
-	 * This is the ArrayList that will be used for this ADT.
+	 * This is the object array that will be used for this ADT.
 	 * All items/nodes will be stored here.
 	 */
-	private ArrayList<E> list;
+	private Object[] list;
+	
+	/**
+	 * This is the default size of the array that will be initialized in the constructor.
+	 */
+	private final static int DEFAULT_SIZE = 10;
+	
+	/**
+	 * This is used to keep track of how many items are currently within the list.
+	 * Keep in mind arrays start at 0, and tracks how many items there are.
+	 * Therefore, size will always be +1 to the largest index in array.
+	 */
+	private int size;
 	
 	/**
 	 * Constructor Method
-	 * Initialize the list by creating an ArrayList<E>.
+	 * Initialize the list by creating an Object[] with default size.
+	 * 
+	 * @author Mikael Ly
+	 * @date 11/5/2025
 	 */
 	public MyArrayList() {
-		this.list = new ArrayList<E>(); 
+		this.list = new Object[DEFAULT_SIZE];
+		this.size = 0;
+	}
+	
+	/**
+	 * Constructor Method if given a size.
+	 * Initialize the list by creating an Object[] with given size.
+	 * (truthfully, size shouldn't matter as this ADT will be of flexible size)
+	 * 
+	 * @author Mikael Ly
+	 * @date last changes made: 11/5/2025
+	 * 
+	 * @param size = size of the array you want to make
+	 */
+	public MyArrayList(int size) {
+		this.list = new Object[size];
+		this.size = 0;
 	}
 
 	
@@ -76,21 +113,42 @@ public class MyArrayList<E> implements ListADT<E> {
 	}
 
 	/**
+	 * This returns an element in the array given an index.
+	 * (suppressed warning unchecked data-type cast)
 	 * 
+	 * @author Mikael Ly
+	 * @date last changes made: 11/5/2025
+	 * 
+	 * @param index - index of the item user wants to retrieve
+	 * @return data of the item at the given index
 	 */
 	@Override
+	@SuppressWarnings("unchecked")
 	public E get(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
+		// TODO Implement get method
+		
+		// If the index is below 0 or greater than the amount of items in the list,
+		// throw an IndexOutOfBoundsException.
+		if (index < 0 || index >= this.size) {
+			throw new IndexOutOfBoundsException("Index is out of bounds. Index: " + index 
+											+ " should be between 0 - " + this.size());
+		}
+		
+		// If index valid, return the item at the given index.
+		return (E) list[index];
 	}
 
 	/**
-	 * This method removes an item from the ArrayList given an index.
-	 * Index must be greater than 0 and no greater than the ArrayList size.
+	 * This method removes an item from the list given an index.
+	 * Index must be greater than 0 and no greater than the list size.
 	 * <p>
 	 * Returns the data stored in the item that was removed.
+	 * <p>
+	 * The way this function works is that it pushes the item to be removed all the way to 
+	 * the end of the list, and then sets it to null.
+	 * 
 	 * @author Mikael Ly
-	 * @date 11/4/2025
+	 * @date last changes made: 11/5/2025
 	 * 
 	 * @param index - index of the item to be removed
 	 * @return data of the item that was removed
@@ -99,27 +157,65 @@ public class MyArrayList<E> implements ListADT<E> {
 	public E remove(int index) throws IndexOutOfBoundsException {
 		// If the index is below 0 or greater than the amount of items in the list,
 		// throw an IndexOutOfBoundsException.
-		if (index < 0 || index >= this.list.size()) {
+		if (index < 0 || index >= this.size) {
 			throw new IndexOutOfBoundsException("Index is out of bounds. Index: " + index 
-											+ " should be between 0 - " + this.list.size());
+											+ " should be between 0 - " + this.size());
 		}
+		
+		// If the list is empty, return null because theres nothing to remove.
+		if (isEmpty()) {
+			return null;
+		}
+				
 		// If index is valid, go ahead with removing the item.
 		// Store item data to be returned
-		E temp = this.list.get(index);
+		E temp = get(index);
 		
-		// Remove the item
-		this.list.remove(index);
+		// Remove the item by shifting all items (to the right of index) to the left by 1
+		// (for starting point of index, iterate through til the end of the list)
+		for (int i = index; i < size - 1; i++) {
+			this.list[i] = this.list[i+1]; // replace current element at pointer with the next element
+		}
+		
+		this.list[size - 1] = null; // remove the element (which was pushed all the way to the end)
 		
 		return temp; // return the data that was removed from the list
 	}
 
 	/**
-	 * @todo To be done by: Mikael
+	 * Removes an element from the list given an element.
+	 * <p>
+	 * This implements a sorting algorithm to compare through all elements in the Array, 
+	 * and removes the given element from the array.
+	 * 
+	 * @author Mikael Ly
+	 * @date last changes made: 11/5/2025
+	 * 
+	 * @param toRemove - the element to remove from the list
+	 * @return element that was just removed, {@code null} if not found
 	 */
 	@Override
 	public E remove(E toRemove) throws NullPointerException {
-		// TODO Auto-generated method stub
-		return null;
+		// TODO do what da summary says
+		
+		// Given element cannot be null, if null, throw an exception.
+		if (toRemove == null) {
+			throw new NullPointerException("Null element cannot be removed.");
+		}
+		
+		// If the list is empty, return null because theres nothing to remove.
+		if (isEmpty()) {
+			return null;
+		}
+		
+		// iterate through the list to find the element
+		for (int i = 0; i < size; i++) {
+			if (toRemove.equals(this.list[i])) { // check if the element at this pointer is equal to the given element
+				return remove(i); // call the remove(index) method to remove that element at that index
+			}
+		}
+		
+		return null; // if code made it to this point, element was not found within the list
 	}
 
 	/**
@@ -132,12 +228,18 @@ public class MyArrayList<E> implements ListADT<E> {
 	}
 
 	/**
-	 * @todo To be done by: Mikael
+	 * Returns whether the list is empty or not. (if size is greater than 0, it's not empty.)
+	 * 
+	 * @author Mikael Ly
+	 * @date last changes made: 11/5/2025
+	 * 
+	 * @return {@code true} if empty, {@code false} if item stil contains items.
 	 */
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		// ternary operator that returns a bool based on condition
+		// (condition) ? if condition true : if condition false
+		return (this.size > 0) ? false : true;
 	}
 
 	/**
