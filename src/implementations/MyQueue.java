6be344f27b117f75bac876fd3342dbbb68dd0d9d
Mikael Ly <mikael2aa@gmail.com>
2025-11-26 22:31:34 -0700

    beginning implementation of MyQueue

--- src/implementations/MyQueue.java
+++ src/implementations/MyQueue.java
@@ -1,80 +1,176 @@
 package implementations;
 
+import java.util.NoSuchElementException;
+
 import utilities.Iterator;
 import utilities.QueueADT;
 
 public class MyQueue<E> implements QueueADT<E> {
+	
+	private final int DEFAULT_CAPACITY = 10;
+	
+	private E[] elements;
+	private int front = 0;
+	private int rear = 0;
+	private int count = 0;
+	private int capacity;
+	
+	/**
+	 * Constructor method for MyQueue (given a size)
+	 * 
+	 * Create a new QueueADT, initializing with a 
+	 * @param capacity
+	 */
+	@SuppressWarnings("unchecked")
+	public MyQueue(int capacity) {
+		this.capacity = capacity;
+		this.elements = (E[]) new Object[this.capacity];
+		this.front = 0;
+		this.rear = -1;
+		this.count = 0;
+	}
+	
+	/**
+	 * Constructor method for MyQueue (not given a size)
+	 * 
+	 * Create a new QueueADT, initializing with a 
+	 * @param capacity
+	 */
+	@SuppressWarnings("unchecked")
+	public MyQueue() {
+		this.capacity = DEFAULT_CAPACITY;
+		this.elements = (E[]) new Object[this.capacity];
+		this.front = 0;
+		this.rear = -1;
+		this.count = 0;
+	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public void enqueue(E object) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public E dequeue() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public E dequeueAll() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public E peek() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
 
+	/**
+	 * Returns a boolean whether this queue is full or not.
+	 */
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public boolean contains(E item) {
-		// TODO Auto-generated method stub
+		
 		return false;
 	}
 
+	/**
+	 * Iterator over the queue from front to rear (head to tail).
+	 * Utilizes the custom QueueIterator class.
+	 */
 	@Override
 	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return new QueueIterator();
+	}
+	
+	/**
+	 * A custom Iterator for QueueADT's.
+	 * Extends from an Iterator object.
+	 */
+	private class QueueIterator implements Iterator<E> {
+		@Override
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public E next() throws NoSuchElementException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public int search(E item) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public E[] toArray(E[] array) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public E[] toArray() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
